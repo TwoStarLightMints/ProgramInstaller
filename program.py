f16ae19ca6ad47448014b70dc3958e21a0616021
KC Bomonti <mondo.beyondo3@gmail.com>
2023-01-09 13:36:44 -0500

    Create init for Program class and declare install()

--- program.py
+++ program.py
@@ -2,3 +2,9 @@ class Program:
     def __init__(self, download_link, program_name) -> None:
         self.download_link: str = download_link
         self.program_name: str = program_name
+    
+    def install() -> int:
+        """
+        Called in order to install the program from the download link
+        """
+        pass
