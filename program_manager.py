c5369596a26ebd944f8c6f84819fda069adede23
KC Bomonti <mondo.beyondo3@gmail.com>
2023-01-09 16:41:10 -0500

    Add function declarations to ProgramManager

--- program_manager.py
+++ program_manager.py
@@ -3,3 +3,13 @@ from tempfile import TemporaryDirectory
 class ProgramManager:
     def __init__(self):
         self._temp_dir = TemporaryDirectory()
+        self.program_list: list[str]
+    
+    def get_progrms(self):
+        pass
+
+    def write_programs(self):
+        pass
+
+    def print_options(self):
+        pass
