b956cd2ba9e8e5709c82c6c23e5cfcb038813c74
KC Bomonti <mondo.beyondo3@gmail.com>
2023-01-09 13:49:58 -0500

    Create initial structure for install()

--- program.py
+++ program.py
@@ -1,10 +1,17 @@
+import requests
+
 class Program:
     def __init__(self, download_link, program_name) -> None:
         self.download_link: str = download_link
         self.program_name: str = program_name
     
-    def install() -> int:
+    def install(self) -> int:
         """
         Called in order to install the program from the download link
         """
-        pass
+        req = requests.get(self.download_link)
+        
+        if not req.ok:
+            print(f"Install attempt resulted in bad status code: {req.status_code}")
+            return 1
+        
