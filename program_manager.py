f0be4f332b0f391ef5e96f2101aded5a54fe87bb
KC Bomonti <mondo.beyondo3@gmail.com>
2023-01-10 11:07:04 -0500

    Define download_setups() in program_manager and add docstrings

--- program_manager.py
+++ program_manager.py
@@ -1,4 +1,5 @@
 from tempfile import TemporaryDirectory
+from os import listdir
 import requests
 import sqlite3
 from program import Program
@@ -13,6 +14,9 @@ class ProgramManager:
         self.program_list: list[Program] = list()
     
     def get_programs(self):
+        """
+        Reads all programs currently stored in the local sqlite3 database and loads them into program list
+        """
         sql_con = sqlite3.connect("install_programs.db")
         sql_cur = sql_con.cursor()
 
@@ -22,6 +26,9 @@ class ProgramManager:
             self.program_list.append(Program(row[1], row[2]))
 
     def write_programs(self):
+        """
+        Writes all programs currently in the program list to the local sqlite3 database.
+        """
         sql_con = sqlite3.connect("install_programs.db")
         sql_cur = sql_con.cursor()
         
@@ -40,10 +47,16 @@ class ProgramManager:
         sql_con.commit()
     
     def show_programs(self):
+        """
+        Prints out all the programs in the program list using their to string method.
+        """
         for program in self.program_list:
             print(program)
         
     def add_program(self):
+        """
+        Exposes functionality to add a program to the list of programs.
+        """
         prog_name = input("Please input a name for the program: ")
         link_ready = False
         link = ""
@@ -61,6 +74,17 @@ class ProgramManager:
                     link_ready = True
                 else:
                     print("Download link responded with an error code, please check your spelling or enter a different link.")
+    
+    def download_setups(self):
+        """
+        Calls the install method of all programs in the program list to download their executables and then uses system to start the process.
+        """
+        print("Retrieving exectuables for programs...")
+        
+        for program in self.program_list:
+            program.install()
+
+        listdir(self._temp_dir)
 
 if __name__ == "__main__":
     pass
