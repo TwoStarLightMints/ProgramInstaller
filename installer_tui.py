d640ca7a77655f1d9c5f18ca7b4e18149476f5de
KC Bomonti <mondo.beyondo3@gmail.com>
2023-01-09 22:05:15 -0500

    Implement get_option() and declare menu()

--- installer_tui.py
+++ installer_tui.py
@@ -5,6 +5,18 @@ class TUI_Mngr:
     def __init__(self) -> None:
         self.help()
 
+    def menu(self):
+        print("-- PROGRAM INSTALLER --")
+    
+    def get_option(self, prompt: str):
+        resp = input(prompt)
+
+        try:
+            return int(resp)
+        
+        except ValueError:
+            return resp
+
     def help(self):
         print("Program Installer")
         print("\nThis program is a TUI which allows you to create a list of programs including their download links for you to be able to easily install those programs all at once.")
