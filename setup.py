d78b474122fc509dfdd283190bf01839d559cca2
KC Bomonti <mondo.beyondo3@gmail.com>
2023-02-10 23:03:43 -0500

    All pip installing is ready for clients

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
-# requests
+from os import system
+
+_DEPENDENCIES = {"requests"}
+
 def install_dependencies():
-    pass
+    if "y" == input(f"Are you sure you want to download all {len(_DEPENDENCIES)}? (y/n) "):
+        for dependency in _DEPENDENCIES:
+            input(f"Now installing {dependency}, continue...")
+            system(f"pip install {dependency}")
+    else:
+        print("Install aborted")
 
 if __name__ == "__main__":
-    pass
+    install_dependencies()
