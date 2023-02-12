ab2a0738bda523466c3a7f88df9caf9d6374618b
KC Bomonti <mondo.beyondo3@gmail.com>
2023-02-12 11:46:04 -0500

    Edit install() to be compatible with properly formatted github api urls

--- program.py
+++ program.py
@@ -1,11 +1,13 @@
 import requests
 from os.path import join
+from urllib.parse import urlparse
 
 class Program:
     def __init__(self, download_link: str, program_name: str, temp_dir: str) -> None:
         self.download_link: str = download_link
         self.program_name: str = program_name
         self.temp_path: str = join(temp_dir, f"{program_name}_SETUP.exe")
+        self._is_github_repo = "github" in self.download_link
     
     def __str__(self) -> str:
         if len(self.download_link) > 60:
@@ -20,6 +22,25 @@ class Program:
         Called in order to install the program from the download link
         """
         print("Sending HTTP request...")
+
+        if self._is_github_repo:
+            # Check that the link is using the proper api
+            if urlparse(self.download_link).netloc.startswith("api"):
+                # Get the assets from the github api
+                repo_assets = requests.get(self.download_link).json()["assets"]
+
+                for asset in repo_assets:
+                    if ".exe" in asset["name"]:
+                        with open(self.temp_path, "wb") as file:
+                            file.write(requests.get(asset["browser_download_url"]).content)
+                        return
+                    
+                    elif "installer" in asset["name"].lower():
+                        backup = asset
+                
+                with open(self.temp_path, "wb") as file:
+                    file.write(requests.get(backup["browser_download_url"]).content)
+
         try:
             req = requests.get(self.download_link)
         except (requests.exceptions.MissingSchema, requests.exceptions.InvalidURL):
